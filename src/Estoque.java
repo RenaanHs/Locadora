fe11311ef89604ed3fd3c52f381fff1d621f2bd5
Antoniele20 <144742273+Antoniele20@users.noreply.github.com>
2024-06-18 00:10:08 -0300

    Update Estoque.java

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,2 +1,10 @@
 public class Estoque {
+    public static boolean verificarDisponibilidade(String titulo) {
+        for (Filme filme : Filme.filmes) {
+            if (filme.titulo.equalsIgnoreCase(titulo) && filme.status == Status.Disponivel) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
