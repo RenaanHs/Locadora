763e0d07f90a2e6d3cb556d3fa2311c64f76bbe7
Spooky662 <104535161+spooky662@users.noreply.github.com>
2024-06-19 16:16:18 -0300

    Update Devolver.java

--- src/Devolver.java
+++ src/Devolver.java
@@ -2,4 +2,8 @@ public class Devolver {
     void devolver(Filme f){
         f.status = Status.Disponivel;
     }
+
+    void devolverSerie(Series s){
+        s.status = Status.Disponivel;
+    }
 }
