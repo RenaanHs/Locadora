bc4c34e4feed76cd1c4397cca325dc25f23d7519
Antoniele20 <144742273+Antoniele20@users.noreply.github.com>
2024-06-20 12:41:08 -0300

    Update Estoque.java

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estoque {
+
+    // Método para verificar a disponibilidade de um filme
     public static boolean verificarDisponibilidade(String titulo) {
         for (Filme filme : Filme.filmes) {
             if (filme.titulo.equalsIgnoreCase(titulo) && filme.status == Status.Disponivel) {
@@ -6,5 +11,13 @@ public class Estoque {
             }
         }
         return false;
+    }
+
+    // Método para listar todos os filmes disponíveis no estoque
+    public static void exibirCatalogoCompleto() {
+        for (Filme filme : Filme.filmes) {
+            System.out.println("Título: " + filme.titulo + ", Categoria: " + filme.categoria +
+                               ", Classificação Indicativa: " + filme.classIndicativa + ", Status: " + filme.status);
+        }
     }
 }
