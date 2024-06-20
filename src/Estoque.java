1368e6dcfffad803613f2cdf965bad7db66ec641
Antoniele20 <144742273+Antoniele20@users.noreply.github.com>
2024-06-20 13:39:09 -0300

    Update Estoque.java

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Estoque {
-    public static boolean verificarDisponibilidadeFilme(String titulo) {
+
+ public static List<Filme> filmes = new ArrayList<>();
+ public static List<Series> series = new ArrayList<>();
+    
+    //disponibilidade de filmes no estoque
+    public static boolean DisponibilidadeFilme(String titulo) {
         for (Filme filme : filmes) {
             if (filme.titulo.equalsIgnoreCase(titulo) && filme.status == Status.Disponivel) {
                 return true;
@@ -7,7 +15,8 @@ public class Estoque {
         }
         return false;
     }
-    public static boolean verificarDisponibilidadeSerie(String titulo) {
+    //disponibilidade de series no estoque
+    public static boolean DisponibilidadeSerie(String titulo) {
         for (Series serie : series) {
             if (serie.titulo.equalsIgnoreCase(titulo) && serie.status == Status.Disponivel) {
                 return true;
@@ -15,23 +24,29 @@ public class Estoque {
         }
         return false;
     }
-    public static void exibirCatalogoCompleto() {
-        for (Filme filme : Filme.filmes) {
-            if (filme.status == Status.Disponivel){
-            System.out.println("Título: " + filme.titulo + ", Categoria: " + filme.categoria + ", Classificação Indicativa: " + filme.classIndicativa + ", Status: " + filme.status);
-            encontrouFilme = true;    
+     //lista filmes disponiveis no estoque
+     public static void listarFilmesDisponiveis() {
+        boolean encontrouFilmes = false;
+        System.out.println("Filmes Disponíveis:");
+        for (Filme filme : filmes) {
+            if (filme.status == Status.Disponivel) {
+                System.out.println("Título: " + filme.titulo + ", Categoria: " + filme.categoria +
+                                   ", Classificação Indicativa: " + filme.classIndicativa + ", Status: " + filme.status);
+                encontrouFilmes = true;
+            }
         }
-        }
-        if (!encontrouFilme) {
-            System.out.println("Nenhum Filme disponível no momento.");
+        if (!encontrouFilmes) {
+            System.out.println("Nenhum filme disponível no momento.");
         }
     }
+    //lista series disponiveis no estoque
     public static void listarSeriesDisponiveis() {
         boolean encontrouSeries = false;
         System.out.println("Séries Disponíveis:");
         for (Series serie : series) {
             if (serie.status == Status.Disponivel) {
-                System.out.println("Título: " + serie.titulo + ", Categoria: " + serie.categoria +", Classificação Indicativa: " + serie.classInficativa + ", Temporadas: " + serie.temporadas +", Status: " + serie.status);
+                System.out.println("Título: " + serie.titulo + ", Categoria: " + serie.categoria +
+                        ", Classificação Indicativa: " + serie.classInficativa + ", Temporadas: " + serie.temporadas +", Status: " + serie.status);
                 encontrouSeries = true;
             }
         }
@@ -39,6 +54,30 @@ public class Estoque {
             System.out.println("Nenhuma série disponível no momento.");
         }
     }
+    //carrega filmes no estoque
+    public static void carregarFilmes(List<Filme> listaFilmes) {
+        filmes.addAll(listaFilmes);
+    }
 
+    //carrega séries no estoque
+    public static void carregarSeries(List<Series> listaSeries) {
+        series.addAll(listaSeries);
+    }
+
+    //exibi o catálogo completo de filmes
+    public static void exibirCatalogoCompletoFilmes() {
+        for (Filme filme : filmes) {
+            System.out.println("Título: " + filme.titulo + ", Categoria: " + filme.categoria +
+                               ", Classificação Indicativa: " + filme.classIndicativa + ", Status: " + filme.status);
+        }
+    }
+
+    //exibi o catálogo completo de séries
+    public static void exibirCatalogoCompletoSeries() {
+        for (Series serie : series) {
+            System.out.println("Título: " + serie.titulo + ", Categoria: " + serie.categoria +
+                               ", Classificação Indicativa: " + serie.classInficativa + ", Status: " + serie.status +
+                               ", Temporadas: " + serie.temporadas);
+        }
+    }
 }
-
