c3b5d94669a35b559d4e87d2d98aac972b8750a6
Antoniele20 <144742273+Antoniele20@users.noreply.github.com>
2024-06-20 13:15:13 -0300

    Update Estoque.java

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,23 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Estoque {
-
-    // Método para verificar a disponibilidade de um filme
-    public static boolean verificarDisponibilidade(String titulo) {
-        for (Filme filme : Filme.filmes) {
+    public static boolean verificarDisponibilidadeFilme(String titulo) {
+        for (Filme filme : filmes) {
             if (filme.titulo.equalsIgnoreCase(titulo) && filme.status == Status.Disponivel) {
                 return true;
             }
         }
         return false;
     }
-
-    // Método para listar todos os filmes disponíveis no estoque
+    public static boolean verificarDisponibilidadeSerie(String titulo) {
+        for (Series serie : series) {
+            if (serie.titulo.equalsIgnoreCase(titulo) && serie.status == Status.Disponivel) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void exibirCatalogoCompleto() {
         for (Filme filme : Filme.filmes) {
-            System.out.println("Título: " + filme.titulo + ", Categoria: " + filme.categoria +
-                               ", Classificação Indicativa: " + filme.classIndicativa + ", Status: " + filme.status);
+            if (filme.status == Status.Disponivel){
+            System.out.println("Título: " + filme.titulo + ", Categoria: " + filme.categoria + ", Classificação Indicativa: " + filme.classIndicativa + ", Status: " + filme.status);
+            encontrouFilme = true;    
+        }
+        }
+        if (!encontrouFilme) {
+            System.out.println("Nenhum Filme disponível no momento.");
         }
     }
+    public static void listarSeriesDisponiveis() {
+        boolean encontrouSeries = false;
+        System.out.println("Séries Disponíveis:");
+        for (Series serie : series) {
+            if (serie.status == Status.Disponivel) {
+                System.out.println("Título: " + serie.titulo + ", Categoria: " + serie.categoria +", Classificação Indicativa: " + serie.classInficativa + ", Temporadas: " + serie.temporadas +", Status: " + serie.status);
+                encontrouSeries = true;
+            }
+        }
+        if (!encontrouSeries) {
+            System.out.println("Nenhuma série disponível no momento.");
+        }
+    }
+
 }
+
