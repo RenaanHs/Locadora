592462091eb455acce08f1e2ec7278e00aadfcb5
RenaanHs <144742314+RenaanHs@users.noreply.github.com>
2024-06-22 12:54:48 -0300

    Update Renan Devolver.java

--- src/Devolver.java
+++ src/Devolver.java
@@ -1,9 +1,11 @@
 public class Devolver {
-    void devolver(Filme f){
-        f.status = Status.Disponivel;
+    public void devolver(Filme f) {
+        f.setStatus(Status.Disponivel);
+        System.out.println("Filme " + f.getTitulo() + " devolvido com sucesso.");
     }
 
-    void devolverSerie(Series s){
-        s.status = Status.Disponivel;
+    public void devolverSerie(Series s) {
+        s.setStatus(Status.Disponivel);
+        System.out.println("Série " + s.getTitulo() + " devolvida com sucesso.");
     }
 }
