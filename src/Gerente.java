fe25bbe868d7650bb42cb5dd3df2ba363a9756a0
RenaanHs <144742314+RenaanHs@users.noreply.github.com>
2024-06-23 14:53:10 -0300

    Update Renan  Gerente.java

--- src/Gerente.java
+++ src/Gerente.java
@@ -2,4 +2,9 @@ public class Gerente extends Funcionario{
     public Gerente(int idade, String nome, String cpf, char genero, String cargo, float salario) {
         super(idade, nome, cpf, genero, cargo, salario);
     }
+
+    @Override
+    public void realizarAcao() {
+        System.out.println("Gerente está gerenciando operações...");
+    }
 }
