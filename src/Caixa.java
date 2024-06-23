d56d024eb0216d46aaf656efb8471c510fc79293
RenaanHs <144742314+RenaanHs@users.noreply.github.com>
2024-06-23 14:52:22 -0300

    Update Renan Caixa.java

--- src/Caixa.java
+++ src/Caixa.java
@@ -2,4 +2,9 @@ public class Caixa extends Funcionario{
     public Caixa(int idade, String nome, String cpf, char genero, String cargo, float salario) {
         super(idade, nome, cpf, genero, cargo, salario);
     }
+
+    @Override
+    public void realizarAcao() {
+        System.out.println("Caixa está processando pagamento...");
+    }
 }
