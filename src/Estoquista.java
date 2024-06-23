e604c71b83d6fc049870626f6e5d3d1922aa7865
RenaanHs <144742314+RenaanHs@users.noreply.github.com>
2024-06-23 14:52:44 -0300

    Update Renan Estoquista.java

--- src/Estoquista.java
+++ src/Estoquista.java
@@ -2,4 +2,9 @@ public class Estoquista extends Funcionario{
     public Estoquista(int idade, String nome, String cpf, char genero, String cargo, float salario) {
         super(idade, nome, cpf, genero, cargo, salario);
     }
+
+    @Override
+    public void realizarAcao() {
+        System.out.println("Estoquista está controlando estoque...");
+    }
 }
