3cb0cf85d3caddea49c40dec73283458b94f76f3
RenaanHs <144742314+RenaanHs@users.noreply.github.com>
2024-06-23 14:51:26 -0300

    Update Renan Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Funcionario extends Pessoa{
+public abstract class Funcionario extends Pessoa {
 
+    static List<Funcionario> funcionarios = new ArrayList<>();
     String cargo;
     float salario;
 
@@ -10,8 +13,19 @@ public class Funcionario extends Pessoa{
         this.cargo = cargo;
         this.salario = salario;
     }
+    public abstract void realizarAcao ();
 
     public static void ListarFuncionarios(Scanner leitor) {
+        Funcionario gerente = new Gerente(35, "João", "123456789", 'M', "Gerente", 5000);
+        Funcionario repositor = new Repositor(25, "Maria", "987654321", 'F', "Repositor", 2500);
+        Funcionario estoquista = new Estoquista(28, "Pedro", "456789123", 'M', "Estoquista", 3000);
+        Funcionario caixa = new Caixa(30, "Ana", "654321987", 'F', "Caixa", 3500);
+
+        System.out.println("Listagem de Funcionários:");
+        for (Funcionario func : funcionarios) {
+            System.out.printf("Nome: %s | Cargo: %s | ", func.nome, func.cargo);
+            Locadora.realizarAcao();
+        }
 
     }
 }
