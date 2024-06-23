450b9d3323c68e972ea43bcae44bdd845d340390
RenaanHs <144742314+RenaanHs@users.noreply.github.com>
2024-06-23 14:53:29 -0300

    Update Renan Repositor.java

--- src/Repositor.java
+++ src/Repositor.java
@@ -2,4 +2,9 @@ public class Repositor extends Funcionario{
     public Repositor(int idade, String nome, String cpf, char genero, String cargo, float salario) {
         super(idade, nome, cpf, genero, cargo, salario);
     }
+
+    @Override
+    public void realizarAcao() {
+        System.out.println("Repositor está reponto estoque...");
+    }
 }
